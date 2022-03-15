d374402b391f9af5e93f42c6e4bf6f5096db51c3
Azlou77 <nguyenlouis172@gmail.com>
2022-03-15 16:56:04 +0100

    update files

---
+++ CRUD/form.php
@@ -0,0 +1,13 @@
+<?php
+require_once "head.php";
+?>
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CRUD PHP</title>
+    </head>
+<body>
+    
+</body>
+</html>
